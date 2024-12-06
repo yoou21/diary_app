bf23005cb535f01c1cb43b9aee57cfcae6dac3a6
nakamura <170897650+yoou21@users.noreply.github.com>
2024-12-06 22:55:23 +0900

    Replace existing content with new_diary_app setup

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,16 +1,8 @@
-# test/controllers/home_controller_test.rb
-require 'test_helper'
+require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    # ユーザー作成
-    user = User.create!(email: 'test@example.com', password: 'password')
-
-    # ログイン
-    login_as(user)
-
-    # ホームページにアクセス
-    get root_url
-    assert_response :success  # ステータスコードが200 OKであることを確認
+    get home_index_url
+    assert_response :success
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,22 +2,14 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-end
-
-module AuthenticationHelper
-  def login_as(user)
-    post user_session_path, params: { user: { email: user.email, password: user.password } }
+    # Add more helper methods to be used by all tests here...
   end
-end
-
-class ActiveSupport::TestCase
-  include AuthenticationHelper
 end
