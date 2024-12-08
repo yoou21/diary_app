e72e43a8e7ee98b157f274f6220ce01115795db5
nakamura <170897650+yoou21@users.noreply.github.com>
2024-12-08 23:54:40 +0900

    Remove all files from the repository

--- spec/features/home_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Home page", type: :feature do
-  it "displays the welcome message" do
-    visit root_path
-    expect(page).to have_content("Welcome")
-  end
-end
