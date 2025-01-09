c53ce3248222b534a8e9468d27d4da5eb582ee33
nakamura <170897650+yoou21@users.noreply.github.com>
2025-01-10 00:48:48 +0900

    Fix remove_index in RemoveUniqueEmotionPerDayFromEmotions migration

--- db/migrate/20250108115516_remove_unique_emotion_per_day_from_emotions.rb
+++ db/migrate/20250108115516_remove_unique_emotion_per_day_from_emotions.rb
@@ -1,5 +1,5 @@
 class RemoveUniqueEmotionPerDayFromEmotions < ActiveRecord::Migration[7.2]
   def change
-    remove_index :emotions, column: [:user_id, :created_at]
+    remove_index :emotions, name: "index_emotions_on_goal_id", if_exists: true
   end
 end
