1b951ead680873e8def53318a1d4f4adf6fac88a
nakamura <170897650+yoou21@users.noreply.github.com>
2025-01-10 00:54:49 +0900

    Disable RemoveUniqueEmotionPerDayFromEmotions migration

--- db/migrate/20250108115516_remove_unique_emotion_per_day_from_emotions.rb
+++ db/migrate/20250108115516_remove_unique_emotion_per_day_from_emotions.rb
@@ -1,5 +1,6 @@
 class RemoveUniqueEmotionPerDayFromEmotions < ActiveRecord::Migration[7.2]
   def change
-    remove_index :emotions, name: "index_emotions_on_goal_id", if_exists: true
+    # マイグレーションを無効化
+    puts "Skipping RemoveUniqueEmotionPerDayFromEmotions migration."
   end
 end
